06ddcd5f487ffbb343abae2bd2b8c9a62ef46f47
Liam DeVoe <orionldevoe@gmail.com>
2018-08-10 10:22:42 -0400

    pull first

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git pull
 javac -cp "dependencies/*:src/main/java" src/main/java/com/tybug/carboncopier/CarbonCopier.java -d sources/ # Compile to class files
 java -cp "./sources:./dependencies/*" com.tybug.carboncopier.CarbonCopier # Run the main class
