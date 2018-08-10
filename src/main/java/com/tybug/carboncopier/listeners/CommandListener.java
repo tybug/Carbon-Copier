adc0586d2bbf3581fb275cdcbafad968faff1d14
Liam DeVoe <orionldevoe@gmail.com>
2018-08-10 10:21:15 -0400

    test commit

--- src/main/java/com/tybug/carboncopier/listeners/CommandListener.java
+++ src/main/java/com/tybug/carboncopier/listeners/CommandListener.java
@@ -1,23 +1,32 @@
 package com.tybug.carboncopier.listeners;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import com.tybug.carboncopier.DBFunctions;
 import com.tybug.carboncopier.Hub;
 
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.HierarchyException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class CommandListener extends ListenerAdapter {
 
-	private static final String SCRIPT_PATH = "./compiler.sh";
-	
-	
+	private static final String SCRIPT_RESTART = "./compiler.sh"; 
+	private static final String GITHUB_CC = "477476287540232202"; // Channel the github webhook is linked to for the Carbon Copier repo
+
+
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		if(event.getChannel().getId().equals(GITHUB_CC)) {
+			restart(event.getJDA());
+		}
+	}
+
+
 	@Override
 	public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
 		if(!DBFunctions.getAuthorizedUsers().contains(event.getAuthor().getId())) {
@@ -36,24 +45,13 @@ public class CommandListener extends ListenerAdapter {
 
 			Hub.linkGuilds(event.getJDA(), event.getChannel(), parts[1], parts[2]);
 		}
-		
-		if(content.startsWith("!restart")) {
-			event.getChannel().sendMessage("Restarting...").queue();
-			 try {
-				Runtime.getRuntime().exec(SCRIPT_PATH);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			 
-			 event.getJDA().shutdown();
-		}
 
 
 		if(content.startsWith("!purge")) {
 			//Quick and dirty, reset a guild for testing
 			Guild guild = event.getJDA().getGuildById(content.split(" ")[1]);
-			
-			
+
+
 			for(Role r : guild.getRoles()) {
 				try {
 					r.delete().queue();
@@ -61,12 +59,26 @@ public class CommandListener extends ListenerAdapter {
 					// Just leave the highest role, can be deleted manually (plus he needs it to delete everything else)
 				}
 			}
-			
+
 			guild.getTextChannels().forEach(channel -> channel.delete().queue());
 			guild.getVoiceChannels().forEach(channel -> channel.delete().queue());
 			guild.getCategories().forEach(channel -> channel.delete().queue());
 
 		}
+	}
+
+
+	
+
+	private static void restart(JDA jda){
+		try {
+			Runtime.getRuntime().exec(SCRIPT_RESTART);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		jda.shutdown();
+
 	}
 
 }
