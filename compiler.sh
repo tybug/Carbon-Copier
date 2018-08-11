1dc820af3411b8ec00a9a27ce84e136b9a36ca44
Liam DeVoe <orionldevoe@gmail.com>
2018-08-10 22:56:32 -0400

    output git pull to log

--- compiler.sh
+++ compiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git pull
+git pull &>> log.txt
 javac -cp "dependencies/*:src/main/java" src/main/java/com/tybug/carboncopier/CarbonCopier.java -d sources/ # Compile to class files
 nohup java -cp "./sources:./dependencies/*" com.tybug.carboncopier.CarbonCopier &>> log.txt # Run the main class, redirect stdout and stderr to log.txt
