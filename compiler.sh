49d9dcf6bc55849f06d769acee70e8522442acc0
Liam DeVoe <orionldevoe@gmail.com>
2018-08-10 20:32:56 -0400

    ignore SIHUP

--- compiler.sh
+++ compiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 javac -cp "dependencies/*:src/main/java" src/main/java/com/tybug/carboncopier/CarbonCopier.java -d sources/ # Compile to class files
-java -cp "./sources:./dependencies/*" com.tybug.carboncopier.CarbonCopier &>> log.txt # Run the main class, redirect stdout and stderr to log.txt
+nohup java -cp "./sources:./dependencies/*" com.tybug.carboncopier.CarbonCopier &>> log.txt # Run the main class, redirect stdout and stderr to log.txt
