ff6013febda39fa44c4dc7b8c52db5ab5a9ce54c
Liam DeVoe <orionldevoe@gmail.com>
2018-08-10 20:25:37 -0400

    redirect logging to file

--- compiler.sh
+++ compiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 javac -cp "dependencies/*:src/main/java" src/main/java/com/tybug/carboncopier/CarbonCopier.java -d sources/ # Compile to class files
-java -cp "./sources:./dependencies/*" com.tybug.carboncopier.CarbonCopier # Run the main class
+java -cp "./sources:./dependencies/*" com.tybug.carboncopier.CarbonCopier &>> log.txt # Run the main class, redirect stdout and stderr to log.txt
